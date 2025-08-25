b97d9fb982362eb8f67a0161821a1805cd336431
Shiril Sukhadeve <chamsukhadeve@gmail.com>
2025-08-26 04:42:17 +0530

    add buffer reopen

--- nvim/lua/plugins/buffer-reopen.lua
+++ nvim/lua/plugins/buffer-reopen.lua
@@ -1,8 +1,4 @@
 return {
     'iamyoki/buffer-reopen.nvim',
-    keys = {
-        -- disable the default keybindings
-        {'<leader>bt', mode={'n'}, false},
-        {'<C-S-t>', mode={'n'}, false},
-    }
+    opts = {}
 }
