c3935dbbc8446fd30d4bb4c72b0d75d9f0ce1867
Shiril Sukhadeve <chamsukhadeve@gmail.com>
2025-08-26 14:01:06 +0530

    update treesj colorizer

--- nvim/lua/plugins/treesj.lua
+++ nvim/lua/plugins/treesj.lua
@@ -1,5 +1,8 @@
 return {
-    "Wansmer/treesj",
-    keys = { '<space>m', '<space>j', '<space>s' },
-    opts = { use_default_keymaps = false, max_join_length = 150 },
+  'Wansmer/treesj',
+  keys = { '<space>m', '<space>j', '<space>s' },
+  dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
+  config = function()
+    require('treesj').setup({--[[ your config ]]})
+  end,
 }
